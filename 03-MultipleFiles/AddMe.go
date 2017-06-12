d93650abc5e5188d073b84eced23ab4d5a5c264f
Taylor Henderson <tph5595@verizon.net>
2017-06-12 14:52:40 -0400

    multiple files working

---
+++ 03-MultipleFiles/AddMe.go
@@ -0,0 +1,7 @@
+package main
+
+import "fmt"
+
+func callMe(name string)  {
+  fmt.Println("Hello, ", name)
+}
---
+++ 03-MultipleFiles/main.go
@@ -0,0 +1,8 @@
+package main
+
+func main()  {
+  //you can write strings on multiple lines with string literals (back ticks)
+  callMe(`
+    Taylor
+    `)
+}
